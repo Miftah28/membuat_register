d85b9d1739c47121f5601a49209b510dc6c1856f
Miftah28 <miftahussurur280602@gmail.com>
2023-05-26 03:53:19 +0700

    update

--- app/Models/Pembeli.php
+++ app/Models/Pembeli.php
@@ -10,6 +10,7 @@ class Pembeli extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'username',
         'alamat',
         'phone'
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,24 +55,24 @@
                             @enderror
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="form-group">
+                            <label for="email" >{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email" tabindex="2" required autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                            
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            {{-- <div class="col-md-6"> --}}
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -80,15 +80,15 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                            {{-- </div> --}}
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            {{-- <div class="col-md-6"> --}}
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                            {{-- </div> --}}
                         </div>
 
                         <div class="row mb-0">
